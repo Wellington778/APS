877c230c76ea30353f0c6d7562865693455ec654
Wellington778 <118468586+Wellington778@users.noreply.github.com>
2023-10-14 14:17:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const fireTable = document.querySelector('.fire-table')
+const latestFireBanner = document.querySelector('.latest-fire')
 
 
+// get information from server
+async function getLatestFire() {
+    const rawData = await fetch('https://smin.wellington777.repl.co/ultimos_incendios')
+    const latestFireData = await rawData.json()
+
+    latestFireBanner.innerHTML = ''
+    latestFireData.forEach(insertLatestFire)
+}
 
 async function getFireInfo() {
     const rawData = await fetch('https://smin.wellington777.repl.co/incendios')
@@ -10,16 +19,24 @@ async function getFireInfo() {
     fireData.forEach(insertTableItem)
 }
 
+// get localizations
+async function getLocal(lat, lon) {
+
+    const rawLocation = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=45644761025242f8a8214fad9611eca8`)
+    const location = await rawLocation.json()
+
+    return location.results[0].components
+}
+
+
+// insert info in HTML
 async function insertTableItem({ acq_date, state_code, latitude, longitude, frp, instrument }) {
 
     const tr = document.createElement('tr')
-
-    const rawLocation = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=45644761025242f8a8214fad9611eca8`)
-    const location = await rawLocation.json()
-    local = location.results[0].components
+    const local = await getLocal(latitude, longitude)
 
     tr.innerHTML = `
-    <td>${acq_date} </td>
+    <td>${acq_date.split('-').reverse().join('/')} </td>
     <td>${local.state_code}</td>
     <td>${local.town || local.village || local.city} </td>
     <td>${frp} </td>
@@ -28,4 +45,15 @@ async function insertTableItem({ acq_date, state_code, latitude, longitude, frp,
     fireTable.appendChild(tr)
 }
 
+async function insertLatestFire({ latitude, longitude, frp }) {
+    const fireData = document.createElement('span')
+
+    const local = await getLocal(latitude, longitude)
+
+    fireData.innerHTML = ` ${local.state_code} <b>${local.town || local.village || local.city}</b> FRP: ${frp} |`
+
+    latestFireBanner.appendChild(fireData)
+}
+
 getFireInfo()
+getLatestFire()
